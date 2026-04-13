555e76969275c5b90d1364ab21f0c76da2f69f37
MinossLunaa <lpbowmanlinehan@email.neit.edu>
2026-04-13 11:05:47 -0400

    end of class week 4

--- Week4/js/script.js
+++ Week4/js/script.js
@@ -2,12 +2,55 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 var fps = 1000/60; //gives you math for 60fps
-var timer = setInterval(game, fps);
-var w = 50;
-var h = 50;
+var timer = setInterval(game, fps); //
+var x = canvas.width/2-50;
+var y = canvas.width/2-50;
+var moveX = setRandomDirection();
+var moveY = setRandomDirection();
+var color = setRandomColor();
 
 function game()
 {
-    ctx.fillStyle = "red";
-    ctx.fillRect(canvas.width/2, canvas.height/2, w += 2, h += 2);
+    //clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //draw object
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, 50, 50);
+
+    //update values
+    x += moveX;
+    y += moveY;
+
+    //border bouncing
+    if(x > canvas.width - 50){
+        moveX = -2;
+        setRandomColor();
+    }
+    if(x < 0) {
+        moveX = +2;
+        setRandomColor();
+    }
+
+    if(y > canvas.height - 50) {
+        moveY = -2;
+        setRandomColor();
+    }
+    if(y < 0) {
+        moveY = +2;
+        setRandomColor();
+    }
+
+}
+
+function setRandomColor(){
+    color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+}
+
+function setRandomDirection(){
+    dir = Math.random();
+    if(dir > 0.5){
+        return 2;
+    } else {
+        return -2;
+    }
 }
